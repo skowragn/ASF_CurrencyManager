ad30c2f39dc0a46fdc4a983cd64724d55e9dce51
Agnieszka Skowrońska <97020391+skowragn@users.noreply.github.com>
2022-01-14 08:51:14 +0100

    Update CurrencyManagerCore folder to CurrencyManagerService.

--- CurrencyManagerCore/Models/Currency.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CurrencyManagerService.Models
-{
-    public class Currency
-    {
-        public string CurrencyName { get; set; }
-        public string CurrencySymbol { get; set; }
-        public string CurrencyFlag { get; set; }
-    }
-}
